c2d5ed4e38b6de9c36f255a5250fd1130bf3224d
Joanna Anderson and Jason Downing <jason@>
2016-08-10 15:58:38 -0700

    add program to reveal choice of survey and hide the previous results

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
   $("#travel-survey form").submit(function(event){
     var name = $("input#name").val();
@@ -6,6 +8,22 @@ $(document).ready(function(){
     var envir = $("input:radio[name=optionsRadios]:checked").val();
     var when = $("input#when").val();
     var color = $("input#color").val();
+
+    $("#kenya").hide();
+    $("#thailand").hide();
+    $("#france").hide();
+    $("#stay-home").hide();
+
+    if (place === "Africa" && envir === "Mountains") {
+      $("#kenya").show();
+    } else if (place === "Asia" && envir === "Beach") {
+      $("#thailand").show();
+    } else if (place === "Europe" && envir === "City") {
+      $("#france").show();
+    } else {
+      $("#stay-home").show();
+    };
+
 
     event.preventDefault();
   });
